593b169b2c96998b818817f018058223d7cab398
DevJuniorBruno <brunode2001@gmail.com>
2023-04-13 15:30:50 -0300

    update projects

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,10 +1,34 @@
 import './projects.css';
+import mobileVet from '../../assets/img/packatualizado.png';
 
 function Projects(){
     return(
         <div className='container'>
-            <div>
-                Projects
+            <div className='projects'>
+                <div className='titleProjects'>
+                    <h1>
+                        PROJECTS
+                    </h1>
+                </div>
+                <div className='line'></div>
+                <div className='paragraphAbout' >
+                    <p>
+                    Here you will find some of my projects and personal characteristics.
+                    </p>
+                </div>
+
+               <div className='projectsContainer'>
+                <div className='imgs' >
+                <img className='webVet' src={ mobileVet } alt="webVet" />
+                </div>
+                <div className='subscriptions' >
+                    <p>
+                      Here you will find some of my projects and personal characteristics.
+                    </p>
+                </div>
+               </div>
+               
+
             </div>
         </div>
     )
